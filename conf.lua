687700bee4025413c5cf05396a175c81f8d15be9
BengalCat <j.kuleszewicz@gmail.com>
2016-03-23 22:40:39 +0100

    Changed shoot sound effect file; Changed linux build directory

--- conf.lua
+++ conf.lua
@@ -2,7 +2,7 @@
 
 function love.conf(t)
 	t.title = "Space Shooter"
-	t.version = "0.9.2"
+	t.version = "0.10.1"
 	gameScale = 3
 	t.window.width = 64 * gameScale
 	t.window.height = 128 * gameScale
